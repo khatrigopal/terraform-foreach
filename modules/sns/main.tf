f4ec6fce4e4fb885c08dc8c0a8214c14b618a2b6
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-01-29 22:26:16 +0530

    Update main.tf

--- modules/sns/main.tf
+++ modules/sns/main.tf
@@ -2,10 +2,3 @@ data "aws_sns_topic" "sns-topic" {
   for_each = var.sns-topics
   name = each.value["sns-topic-name"]
 }
-
-resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
-  for_each =  var.sns-topics
-  topic_arn = data.aws_sns_topic.sns-topic[each.key]
-  protocol  = "email"
-  endpoint  = "khatri.gopal@gmail.com"
-}
