d254789d0cf03eb81d52e0a05fcba8143a324a0e
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-01-29 22:03:06 +0530

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -1,8 +1,28 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+### Backend ###
+# S3
+###############
+
+terraform {
+  backend "s3" {
+    bucket = "khatrig-githubaction"
+    key = "full-resources.tfstate"
+    region = "us-east-1"
+  }
+}
+######### S3##############
 module "s3" {
   source     = "../modules/s3"
   s3-buckets = var.s3-buckets
   }
+##########################
+  
+####### SNS ###################
 module "sns" {
   source     = "../modules/sns"
   sns-topics = var.sns-topics
 }
+#############################
