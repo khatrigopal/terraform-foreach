fd435be46f84c3a37b970c1e9732d5d23c8304a4
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-01-29 21:48:58 +0530

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -2,7 +2,7 @@ module "s3" {
   source     = "../modules/s3"
   s3-buckets = var.s3-buckets
   }
-module "sns" {
+module "sns-topic" {
   source     = "../modules/sns"
   sns-topic-name = var.sns-topics
 }
