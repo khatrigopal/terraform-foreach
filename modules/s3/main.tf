f584328c7d359862ccfea63bf004d2acb15cda10
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-01-29 15:05:28 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,26 +1,7 @@
 resource "aws_s3_bucket" "s3-bucket" {
   for_each = var.s3-buckets
   bucket = each.value["s3_bucket_name"]
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:ListAllMyBuckets"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    },
-    {
-      "Action": [
-        "s3:*"
-      ],
-      "Effect": "Allow",
-      "Resource": aws_s3_bucket.s3-bucket[each.key]
-    }
-  ]
 }
-EOF
-}
+
+
 
